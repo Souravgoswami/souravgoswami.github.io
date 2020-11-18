c49eca87402af98c6bff61e8386b3086136db3ec
Sourav Goswami <souravgoswami@protonmail.com>
2020-11-18 19:48:05 +0530

    Added a code minifier ruby-gem

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ gem "home"
 
 group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.12"
+  gem 'jekyll-strip-html', '0.1.0', path: File.join(__dir__, 'jekyll-strip-html')
 end
--- assets/js/liquidBg.js
+++ assets/js/liquidBg.js
@@ -1,12 +1,13 @@
 $(() => {
 	const liquidBg = document.querySelector('#liquidBg')
-	lBgStyle = liquidBg.style
+	let lBgStyle = liquidBg.style
 	lBgStyle.filter = `hue-rotate(${(70 * (liquidBg.getBoundingClientRect().top / window.innerHeight))}deg)`
 
 	window.addEventListener('scroll', function(e) {
-		new window.IntersectionObserver(e => {
-			if (e[0].isIntersecting)
-				lBgStyle.filter = `hue-rotate(${(70 * (liquidBg.getBoundingClientRect().top / window.innerHeight))}deg)`
-		}).observe(liquidBg)
+		let viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight)
+		let liqBCR = liquidBg.getBoundingClientRect()
+
+		if(viewportHeight > liqBCR.top && liqBCR.top + liqBCR.height > 0)
+			lBgStyle.filter = `hue-rotate(${(70 * (liquidBg.getBoundingClientRect().top / window.innerHeight))}deg)`
 	})
 })
